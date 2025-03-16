d12c449f3993697c9ee8663e81e7c2e39f7cbc37
Keerthi BM <keerthibm22@gmail.com>
2025-03-17 00:37:46 +0530

    google analytics

--- app.js
+++ app.js
@@ -745,12 +745,8 @@ app.post("/upload", upload.single("note"), (req, res) => {
 
 // ------------------------
 // Start the Server
-// ------------------------
-app.listen(PORT, "0.0.0.0", () => {  // Bind to 0.0.0.0
+const server = app.listen(PORT, "0.0.0.0", () => {  
   console.log(`Server is running on port ${PORT}`);
-});
-const server = app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
 });
 
 // Increase timeout settings
